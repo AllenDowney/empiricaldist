88568d5601a9852603e69009364bd7769f88a35f
Allen Downey <downey@allendowney.com>
2020-05-08 19:21:42 -0400

    Removing unneded import

--- empiricaldist/empiricaldist.py
+++ empiricaldist/empiricaldist.py
@@ -16,7 +16,6 @@ MIT License: https://opensource.org/licenses/MIT
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 from scipy.interpolate import interp1d
 
