b5126938074bd4e6a41102e8ac4fa1af165bb10b
John Blischak <jdblischak@gmail.com>
2022-05-04 22:26:29 -0400

    Bump to 0.6.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name="empiricaldist",
-    version="0.6.5",
+    version="0.6.6",
     author="Allen B. Downey",
     author_email="downey@allendowney.com",
     packages=["empiricaldist"],
